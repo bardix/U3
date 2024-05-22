c0ecdb12a72d4ce77a866c3633867475a652affb
Davide Lombroni <davide.lombroni98@gmail.com>
2024-05-22 14:38:06 +0200

    .

--- Settimana_1/Giorno_3/giorno3/src/app/app.module.ts
+++ Settimana_1/Giorno_3/giorno3/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ActivePostsComponent } from './Components/active-posts/active-posts.component';
+import { InactivePostsComponent } from './Components/inactive-posts/inactive-posts.component';
+import { PostDetailComponent } from './Components/post-detail/post-detail.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { HomeComponent } from './Components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActivePostsComponent,
+    InactivePostsComponent,
+    PostDetailComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
