ad984c0dc40bcb6e88ebdc0df7475e6de29544df
Davide Lombroni <davide.lombroni98@gmail.com>
2024-05-22 15:50:26 +0200

    .

--- Settimana_1/Giorno_3/giorno3/src/app/app.component.ts
+++ Settimana_1/Giorno_3/giorno3/src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
-
+import { iArticle } from './Models/article';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'giorno3';
+
+  apiUrl:string = '../../../giorno3/src/assets/db.json';
+
+  userArr:iArticle[] = []
+
+  ngOnInit(){
+
+    this.getUsers()
+
+    //commento questo perché il metodo async getUsers fa la stessa cosa
+    // fetch(this.apiUrl)
+    // .then(res => <Promise<iUser[]>> res.json())
+    // .then(users => {
+    //   this.userArr = users;
+    // })
+
+  }
+
+  async getUsers():Promise<void>{
+
+    let response = await fetch(this.apiUrl)
+    let users = <iArticle[]> await response.json()
+    //uso <iUser[]> per spiegare a ts che mi aspetto oggetti iUser
+
+    setTimeout(()=>{
+      this.userArr = users;
+    }, 5000)
+
+  }
+
 }
