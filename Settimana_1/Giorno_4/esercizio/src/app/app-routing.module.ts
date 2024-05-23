d1806dfdea029178beb2af8a971a96b6584326e7
Davide Lombroni <davide.lombroni98@gmail.com>
2024-05-23 14:29:16 +0200

    .

--- Settimana_1/Giorno_4/esercizio/src/app/app-routing.module.ts
+++ Settimana_1/Giorno_4/esercizio/src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path:'home',
+    component: HomeComponent,
+    title: 'Home'
+  },
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
