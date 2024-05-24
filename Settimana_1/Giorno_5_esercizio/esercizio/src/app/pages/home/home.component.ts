12cfb2dce9b542ce7204da3eb592ec2b09fcc051
Davide Lombroni <davide.lombroni98@gmail.com>
2024-05-24 13:32:28 +0200

    .

--- Settimana_1/Giorno_5_esercizio/esercizio/src/app/pages/home/home.component.ts
+++ Settimana_1/Giorno_5_esercizio/esercizio/src/app/pages/home/home.component.ts
@@ -1,56 +1,45 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
 import { iPost } from '../../Models/ipost';
+
+interface Brand {
+  name: string;
+  logo: string;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
-  postsArr:iPost [] = [];
-  firstPost!:iPost;
-  secondPost!:iPost;
-  randomPostsArr:iPost[] = [];
+  postsArr: iPost[] = [];
+  randomPostsArr: iPost[] = [];
+  uniqueBrands: Brand[] = [];
 
-
-  ngOnInit(){
-
-    this.getPosts().then(()=>{
-
-    let firstPost = this.getFirstPost();
-    let secondPost = this.getSecondPost();
-    if (secondPost) {
-      this.secondPost = secondPost;
-    }
-
-    if (firstPost) {
-      this.firstPost = firstPost;
-    }
-
-    this.randomPostsArr= this.getRandomPost()
-  })
-
-  }
-    async getPosts(){
-      const response = await fetch('../../../assets/db.json')
-      const posts = <iPost[]> await response.json();
-      this.postsArr = posts;
-      console.log(this.postsArr);
-
-    }
-    getSecondPost(){
-      return this.postsArr[1];
-    }
-
-    getFirstPost(){
-      return this.postsArr.shift();
-    }
-
-    getRandomPost() :iPost[] {
-      const shuffled = [...this.postsArr].sort(() => 0.5 - Math.random());
-      return shuffled.slice(0,2);
-    }
+  ngOnInit() {
+    this.getPosts().then(() => {
+      this.randomPostsArr = this.getRandomPost();
+      this.extractUniqueBrands();
+    });
   }
 
+  async getPosts() {
+    const response = await fetch('../../../assets/db.json');
+    this.postsArr = await response.json();
+  }
 
+  getRandomPost(): iPost[] {
+    const shuffled = [...this.postsArr].sort(() => 0.5 - Math.random());
+    return shuffled.slice(0, 2);
+  }
+
+  extractUniqueBrands() {
+    const uniqueBrandNames = new Set(this.postsArr.map(car => car.brand));
+    this.uniqueBrands = Array.from(uniqueBrandNames).map(brandName => ({
+      name: brandName,
+      logo: this.postsArr.find(car => car.brand === brandName)?.brandLogo || ''
+    }));
+  }
+}
