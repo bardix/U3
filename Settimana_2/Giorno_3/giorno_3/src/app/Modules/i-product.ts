f9a92a5942ad9e40fb9441300040555baf73db2c
Davide Lombroni <davide.lombroni98@gmail.com>
2024-05-29 14:53:51 +0200

    .

--- Settimana_2/Giorno_3/giorno_3/src/app/Modules/i-product.ts
+++ Settimana_2/Giorno_3/giorno_3/src/app/Modules/i-product.ts
@@ -1,2 +1,28 @@
-export interface IProduct {
+import { IDimensions } from "./i-dimensions"
+import { IMeta } from "./i-meta"
+import { IReview } from "./i-review"
+
+export interface IProduct{
+  id: number
+  title: string
+  description: string
+  category: string
+  price: number
+  discountPercentage: number
+  rating: number
+  stock: number
+  tags: string[]
+  brand: string
+  sku: string
+  weight: number
+  dimensions: IDimensions
+  warrantyInformation: string
+  shippingInformation: string
+  availabilityStatus: string
+  reviews: IReview[]
+  returnPolicy: string
+  minimumOrderQuantity: number
+  meta: IMeta
+  thumbnail: string
+  images: string[]
 }
