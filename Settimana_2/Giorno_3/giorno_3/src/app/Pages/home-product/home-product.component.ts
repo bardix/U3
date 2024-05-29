d6524f601923eb9343b7032e9e6cd9140653b6e5
Davide Lombroni <davide.lombroni98@gmail.com>
2024-05-29 15:32:24 +0200

    .

--- Settimana_2/Giorno_3/giorno_3/src/app/Pages/home-product/home-product.component.ts
+++ Settimana_2/Giorno_3/giorno_3/src/app/Pages/home-product/home-product.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { ProdottiserviceService } from '../../Services/prodottiservice.service';
+import { IProduct } from '../../Modules/i-product';
+
 
 @Component({
   selector: 'app-home-product',
   templateUrl: './home-product.component.html',
-  styleUrl: './home-product.component.scss'
+  styleUrls: ['./home-product.component.scss']
 })
-export class HomeProductComponent {
+export class HomeProductComponent implements OnInit {
+  prodotti: IProduct[] = [];
+
+
+  constructor(private prodottiService: ProdottiserviceService) { }
+
+
+  ngOnInit() {
+    this.prodottiService.getProdotti().subscribe(data => {
+      this.prodotti = data.products; // Assicurati che la struttura di 'data' corrisponda a quella ricevuta dal tuo backend
+      console.log(this.prodotti);
+    });
+  }
 
 }
--- Settimana_2/Giorno_3/giorno_3/src/app/Services/prodottiservice.service.ts
+++ Settimana_2/Giorno_3/giorno_3/src/app/Services/prodottiservice.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProdottiserviceService {
 
-  constructor() { }
+  private url = 'https://dummyjson.com/products';
+
+  constructor(private http: HttpClient) { }
+
+  getProdotti(): Observable<any> {
+    return this.http.get(this.url);
+  }
 }
+
+
+
