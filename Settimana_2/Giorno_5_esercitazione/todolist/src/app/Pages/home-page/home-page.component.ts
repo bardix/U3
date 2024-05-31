1311a70cf064806c9bbc6b408596d2e1a5b246da
Davide Lombroni <davide.lombroni98@gmail.com>
2024-05-31 13:37:45 +0200

    .

--- Settimana_2/Giorno_5_esercitazione/todolist/src/app/Pages/home-page/home-page.component.ts
+++ Settimana_2/Giorno_5_esercitazione/todolist/src/app/Pages/home-page/home-page.component.ts
@@ -9,16 +9,31 @@ import { TodoServService } from '../../Services/todo-serv.service';
 })
 export class HomePageComponent implements OnInit {
   todosWithUsers: any[] = [];
+  filteredTodos: any[] = [];
+  searchQuery: string = '';
 
   constructor(private datiService: DatiService, private todoService: TodoServService) { }
 
   ngOnInit(): void {
     this.todosWithUsers = this.datiService.getTodosWithUsers();
+    this.filteredTodos = this.todosWithUsers;
   }
 
   toggleCompletion(todo: any): void {
     todo.completed = !todo.completed;
     this.todoService.updateTodoStatus(todo.id, todo.completed);
-    this.todosWithUsers = this.datiService.getTodosWithUsers(); // Aggiorna la lista
+    this.filteredTodos = this.datiService.getTodosWithUsers(); // Aggiorna la lista
+    this.filterTodos(); // Applica il filtro dopo l'aggiornamento
+  }
+
+  filterTodos(): void {
+    if (this.searchQuery) {
+      this.filteredTodos = this.todosWithUsers.filter(todo =>
+        todo.userName.toLowerCase().includes(this.searchQuery.toLowerCase())
+      );
+    } else {
+      this.filteredTodos = this.todosWithUsers;
+    }
   }
 }
+
